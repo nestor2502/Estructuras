ea450739cdad889b871c33f43076a57e271b8d1f
nestor <nestor2502@ciencias.unam.mx>
2019-01-30 20:05:18 -0600

    se agregan autores

--- src/MyString.java
+++ src/MyString.java
@@ -1,8 +1,13 @@
-
+/**
+*@author Nestor Semer Vazquez Cordero
+*@author Isay Balderas Salomon
+*@version 1.0
+*
+*/
 
 public class MyString {
 
-	public String [];
+	public char [];
 
 
 	/** Constructor para generar una cadena vacia */
