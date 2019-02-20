877c69d41e69860488a99870ef85da55c5300c87
nestor2502 <nestor2502@ciencias.unam.mx>
2019-02-20 11:30:07 -0600

    se agregan metodos

--- src/practica2/Lista.java
+++ src/practica2/Lista.java
@@ -1,132 +1,415 @@
-
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+
+    import java.util.Iterator;
+    import java.util.NoSuchElementException;
 
 
-/**
- * Implementación de listas ligadas.
- */
-public class Lista<T> implements Iterable<T>{
+    /**
+     * Implementación de listas ligadas.
+     */
+    public class Lista<T> implements Iterable<T>{
 
-    // Clase interna para representar los nodos de nuestras listas.
-    private class Nodo{
-	
-	public T elemento;
-	public Nodo siguiente;
-	public Nodo anterior;
-	
-	public Nodo(T elemento){
-	    // Aquí va su código.
-	}
-    }
+        ////////////////////////////////////////////////////////////////
 
-    // Clase para iterar la lista.
-    private class Iterador implements Iterator<T>{
+        // Clase interna para representar los nodos de nuestras listas.
+        private class Nodo{
+        
+        public T elemento;
+        public Nodo siguiente;
+        public Nodo anterior;
+        
+        public Nodo(T elemento){
+            
+            this.elemento = elemento;
+        }
 
-	public Nodo siguiente;
+        }
 
-	public Iterador(){
-	    // Aquí va su código.
-	}
 
-	@Override
-	public boolean hasNext(){
-	    // Aquí va su código.
-	}
-	
-	@Override
-	public T next(){
-	    // Aquí va su código.
-	}
-    }
+    ///////////////////////////////////////////////////////////////////////
 
-    private Nodo cabeza;
-    private Nodo ultimo;
-    private int longitud;
-    
-    public Lista(){
-	// Aquí va su código.
-    }
 
-    public Lista(T[] arreglo){
-	// Aquí va su código.
-    }
-    
-    public T getPrimero(){
-	// Aquí va su código.
-    }
+        // Clase para iterar la lista.
+        private class Iterador implements Iterator<T>{
 
-    public T getUltimo(){
+        public Nodo siguiente;
+
+        public Iterador(){
+
+             siguiente = cabeza;
+
+         }
+
+        @Override
+        public boolean hasNext(){
+             
+             return (siguente!=null);
+            
+         }
+        
+         /**
+         *Regresa el siguiente elemento en la iteracion
+         */
+         @Override
+         public T next() throws NoSuchElementException{
+
+            if(siguiente == null){
+                
+             throw new NoSuchElementException();
+
+             }
+
+                else{
+
+                 T aux = siguiente.elemento;
+
+                 siguiente = siguiente.siguiente;
+
+
+                 return aux;
+                 
+                 }      
+        }
+        }
+    //////////////////////////////////////////////////////////////////////////
+
+
+        private Nodo cabeza;
+        private Nodo ultimo;
+        private int longitud=0;
+        
+        /**
+        *Constructor vacio
+        */
+        public Lista(){
+
+        cabeza = null;
+        cabeza.siguiente = ultimo;
+        cabeza.anterior= null;
+        ultimo = null;
+        ultimo.siguiente=null;
+        ultimo.anterior=cabeza;
+
+        }
+
+
+        /**
+        *Constructor que toma un arreglo y mete sus elementos a la lista 
+        */
+        public Lista(T[] arreglo){
+
+             int longitud = arreglo.length;
+
+             if(longitud==0){
+                  cabeza = null;
+                  cabeza.siguiente = ultimo;
+                  cabeza.anterior= null;
+                  ultimo = null;
+                  ultimo.siguiente=null;
+                  ultimo.anterior=cabeza;
+
+                 }
+
+             if (longitud==1){
+
+                 Nodo n1 = new Nodo(arreglo[0]);
+
+                 cabeza= n1;
+                 cabeza.siguiente=null;
+                 cabeza.anterior=null;
+                 ultimo=n1;
+                 ultimo.siguiente=null;
+                 ultimo.anterior=null;
+
+                 longitud ++;
+                 
+                 }
+             
+             if(longitud>1){
+
+                     Nodo n1 = new Nodo(arreglo[0]);
+                     cabeza = n1;
+                     cabeza.anterior= null;
+                     cabeza.siguiente=null;
+                     ultimo=cabeza;
+
+
+                 for(int i= 1;i<longitud;i++ ){
+
+
+                     if(i>=1 && i<longitud){
+                        
+                        Nodo n2= new Nodo(arr[i]);           
+
+                         agregaFinal(n2.elemento);
+                         
+
+                     }
+
+                    
+             }
+          }
+         }
+        
+
+        /**
+        *Regresa el primer elemento de la lista
+        */
+        //si la lista es vacía, deben lanzar una NoSuchElementException.
+        public T getPrimero(){
+        
+             return cabeza.elemento;
+
+         }
+
+
+        /**
+        *Regresa el ultimo elemento de la lista, 
+        */
+        //si la lista es vacía, deben lanzar una NoSuchElementException.
+        public T getUltimo(){
+            
+             return ultimo.elemento;
+
+         }
+        
+
+        /**
+        *Elimina el primer elemento de la lista, si la lista es vacia 
+        *@throws NoSuchElementException 
+        */
+        //debe lanzar NoSuchElementException
+        public T eliminaPrimero() throws NoSuchElementException{
+
+             if(longitud == 0){
+
+                 throw new NoSuchElementException;
+                 }
+
+             if(longitud == 1){
+                
+                 Nodo temp = new Nodo (cabeza.elemento);            
+                 cabeza = null;
+                 cabeza.anterior= null;
+                 cabeza.siguiente = null;
+                 
+
+                 ultimo = null;
+                 ultimo.anterior= null;
+                 ultimo.siguiente = null;
+                 longitud--;
+                 }
+
+             if(longitud > 1){
+
+                Nodo temp = new Nodo (cabeza.elemento);
+
+                 cabeza.anterior= null;
+                 cabeza=cabeza.siguiente;
+
+                 
+                 longitud--;
+                 }
+
+             
+
+                 return temp.elemento;
+         }
+        
+
+        /**
+        *Elimina el ultimo elemento de la lista, si la lista es vacia 
+        *@throws NoSuchElementException
+        */
+        //debe lanzar NoSuchElementException
+        public T eliminaUltimo() throws NoSuchElementException{
+
+             if(longitud==0)
+
+                 throw new NoSuchElementException;
+
+             if(longitud == 1){
+
+                 Nodo temp2 = new Nodo(ultimo.elemento);
+               
+                 ultimo = null;
+
+                 cabeza = null;
+                 
+                 longitud--;
+                 }
+
+             if(longitud > 1){
+
+                 Nodo temp2 = new Nodo(ultimo.elemento);
+
+                 ultimo = ultimo.anterior;
+
+                 ultimo.siguiente = null;
+
+                 longitud--;
+
+                 }
+
+             
+                 return temp2.elemento;
+        
+        }
+        
+
+        /**
+        *Recibe un elemento y lo agrega al inicio de la lista
+        */
+        public void agregaInicio(T t){
+
+             Nodo nuevoElemento = new Nodo(t);
+
+             nuevoElemento.siguiente = cabeza;
+
+             nuevoElemento.anterior= null;
+
+             cabeza.anterior = nuevoElemento;        
+
+        }
+        
+
+        /**
+        *Recibe un elemento y lo agrega al final de la lista
+        */
+        public void agregaFinal(T t){
+
+             Nodo nuevoElemento = new Nodo(t);
+
+             nuevoElemento.siguiente = null;
+
+             nuevoElemento.anterior= ultimo;
+
+             ultimo.siguiente = nuevoElemento; 
+
+        
+        }
+        
+
+        /**
+        *Recibe un elemento, si el elemento se encuentra en la lista devuelve true
+        *si no false 
+        */
+        public boolean contiene(T t){
+
+             
+                
+            if(t == null){
+                return false;
+            }
+            else{
+
+                return true;
+            }
+
+        
+        }
+        
+
+        /**
+        *regresa la longitud de la lista
+        *@return longitud 
+        */
+        public int getLongitud(){
+
+             return longitud;
+
+        }
+        
+
+        /**
+        *Recibe un elemento y elimina su primera ocurrencia en la lista,
+        *devuelve true si el elemento se eliminó, false en otro claso
+        */
+        public boolean elimina(T t){
         // Aquí va su código.
-    }
+        }
+        
 
-    public T eliminaPrimero(){
-	// Aquí va su código.
-    }
+        /**
+        *Vacía la lista
+        */
+        public void limpia(){
+        
+        cabeza= ultimo;
+        cabeza = null;
+        cabeza.siguiente=null;
+        cabeza.anterior= null;
 
-    public T eliminaUltimo(){
-	// Aquí va su código.
-    }
+        }
+        
 
-    public void agregaInicio(T t){
-	// Aquí va su código.
-    }
+        /**
+        *Este método toma un índice i y regresa el i-ésimo elemento de la lista.
+        */
+        //Deben lanzar una IndexOutOfBoundsException si el índice es menor a 0 o mayor o igual al tamaño de la lista.
+        public T get(int indx){
+        // Aquí va su código.
+        }
+        
 
-    public void agregaFinal(T t){
-	// Aquí va su código.
-    }
+        /**
+        *Toma un índice i y un elemento y mete el elemento en la posición i de la lista.
+        //
+        */
+        public void inserta(int indx, T t){
+        // Aquí va su código.
+        }
+        
 
-    public boolean contiene(T t){
-	// Aquí va su código.
-    }
+        /**
+        *Este método nos regresa un arreglo con los mismos elementos de nuestra lista en el mismo orden.
+        */
+        public Object[] toArray(){
+        // Aquí va su código.
+        }
+        
 
-    public int getLongitud(){
-	// Aquí va su código.
-    }
+        /**
+        *Este método nos regresa una lista con los elementos de nuestra lista en orden inverso. 
+        */
+        public Lista<T> reversa(){
+        // Aquí va su código.
+        }
 
-    public boolean elimina(T t){
-	// Aquí va su código.
-    }
+        
 
-    public void limpia(){
-	// Aquí va su código.
-    }
-
-    public T get(int indx){
-	// Aquí va su código.
-    }
- 
-    public void inserta(int indx, T t){
-	// Aquí va su código.
-    }
-
-    public Object[] toArray(){
-	// Aquí va su código.
-    }
-
-    public Lista<T> reversa(){
-	// Aquí va su código.
-    }
+        /**
+        *Este método nos regresa una copia de nuestra lista.
+        */
+        public Lista<T> copia(){
+        // Aquí va su código.
+        }
 
 
-    public Lista<T> copia(){
-	// Aquí va su código.
-    }
-    
-    @Override
-    public String toString(){
-       	// Aquí va su código.
-    }
+        /**
+        *El método regresa una cadena con la información de nuestra lista.
+        */
+        @Override
+        public String toString(){
+            // Aquí va su código.
+        }
 
-    @Override
-    public boolean equals(Object o){
-      	// Aquí va su código.
+        /**
+        *Deben regresar true si el objeto recibido es igual a la lista y false en otro caso.
+        */
+        @Override
+        public boolean equals(Object o){
+            // Aquí va su código.
+        }
+
+
+        /**
+        *Este método pertenece a la interfaz Iterable
+        */
+        @Override
+        public Iterator<T> iterator(){
+        
+        return new Iterador();
+
+        }
     }
-    
-    @Override
-    public Iterator<T> iterator(){
-	// Aquí va su código.
-    }
-}
-Lista.java
-Mostrando Lista.java.
