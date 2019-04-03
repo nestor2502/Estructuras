d75aea131fd55e12f8a9ef8753f21e237bc8e8ca
IsayDBS <42330467+IsayDBS@users.noreply.github.com>
2019-04-03 10:41:30 -0600

    Add files via upload

--- pruebasarboles.java
+++ pruebasarboles.java
@@ -1,5 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class pruebasarboles {
+public class pruebasarboles<T extends Comparable<T>> {
 	public Nodo raiz;
 	public int elementos;
 public pruebasarboles(){
@@ -7,24 +9,24 @@ public pruebasarboles(){
 	this.elementos = 0;
 }
 
-public pruebasarboles(int[] arreglo){
+public pruebasarboles(T[] arreglo){
 	this.elementos = arreglo.length;
 	for(int i=0;i<arreglo.length;i++) {
 		agrega(arreglo[i]);
 	}
 }
 
-public void agrega(int elemento) {
+public void agrega(T elemento) {
 	Nodo nuevoNodo = new Nodo(elemento);
 	if(raiz == null) {
 		raiz = nuevoNodo;
 		
 	}else {
-		Nodo temporal = raiz;	
+		Nodo temporal = raiz;
 		//Hacemos el recorrido para encontrar un lugar vacio
 		while(temporal != null) {
 			nuevoNodo.padre = temporal;
-			if(elemento<=temporal.elemento) {
+			if(elemento.compareTo(temporal.elemento)< 0) {
 				temporal = temporal.izquierdo;
 				
 			}else {
@@ -33,7 +35,7 @@ public void agrega(int elemento) {
 			}			
 		}
 		//Vemos en donde vamos a ponerlo
-		if(nuevoNodo.elemento < nuevoNodo.padre.elemento) {
+		if(nuevoNodo.elemento.compareTo(nuevoNodo.padre.elemento)<=0) {
 			nuevoNodo.padre.izquierdo = nuevoNodo;
 		}else {
 			nuevoNodo.padre.derecho = nuevoNodo;
@@ -42,34 +44,178 @@ public void agrega(int elemento) {
 	}
 	elementos++;
 }
+//-------------------------------------------------------------------
+public Nodo maximo(T z) {
+	Nodo encontrado = buscaNodo(z);
+	while(encontrado.derecho != null) {
+		encontrado = encontrado.derecho;
+	}
+	return encontrado;
+}
+//--------------------------------------------------------------------
+public Nodo predecesor(T y) {
+	Nodo encontrado = buscaNodo(y);
+	//arbol vacio
+	if(encontrado == null)
+		return null;
+	//revisamos que su izquierda no este vacia y buscamos el maximo en ese subarbol
+	if(encontrado.izquierdo != null) {
+	return maximo(encontrado.izquierdo.elemento);
+	}
+	else {
+		//Si no tiene arbol izquierdo, entonces subimos y buscamos a alguien que si lo tenga
+	Nodo aux = encontrado.padre;
+	while(aux != null && encontrado == aux.izquierdo) {
+		encontrado = aux;
+		aux = aux.padre;
+	}
+	
+	if(aux == null)
+		return null;
+	else 
+		return aux;
+	}
+	//return null;
+}
+
 //----------------------------------------------------------------------
-/**public boolean elimina(int j) {
-	//No puedes borrar un arbol vacio
-	if(raiz == null)
+public boolean elimina(T j) {
+	Nodo aux = buscaNodo(j);
+	if(raiz == null) {
 		return false;
-	//Sin hijos
-	Nodo prueba2 = raiz;
-	if(prueba2.izquierdo == null && prueba2.derecho == null) {
-		prueba2 = null;
+	}
+//------------------------------------------------------------------------------
+	//No tiene hijos
+	if(aux.izquierdo == null && aux.derecho == null) {
+		if(aux == raiz) {
+			raiz = null;
+			elementos--;
+			return true;
+		}
+		
+		if(aux.padre.izquierdo == aux)
+			aux.padre.izquierdo = null;
+		else
+			aux.padre.derecho = null;
 		return true;
+	
 	}
+//-------------------------------------------------------------------------------------------------
+//Tiene un nodo hijo
+	//Caso izquierdo
+	if(aux.derecho == null && aux.izquierdo != null) {
+		if(aux == raiz) { //Caso especial
+			raiz = raiz.izquierdo;
+			elementos--;
+			return true;
+		}
+		
+		
+		if(aux.padre.izquierdo == aux)
+			aux.padre.izquierdo = aux.padre.izquierdo;
+		else
+			aux.padre.derecho = aux.padre.izquierdo;
+		
+		return true;
+
+	
+
+	}
+	//Caso derecho
+//--------------------------------------------------------------------	
+	if(aux.izquierdo == null && aux.derecho != null) {
+		if(aux == raiz) {	//Caso especial
+			raiz = raiz.derecho;
+			elementos--;
+			return true;
+		}
+		
+		if(aux.padre.izquierdo == aux)
+			aux.padre.izquierdo = aux.padre.derecho;
+		else
+			aux.padre.derecho = aux.padre.derecho;
+		
+		return true;
+
+	}
+//--------------------------------------------------------------------
+	//Caso tiene 2 hijos
+	if(aux.izquierdo != null && aux.derecho != null) {
+		Nodo predecesor = predecesor(aux.elemento);
+		//Nodo temporal;
+		aux.elemento = predecesor.elemento;
+		if(predecesor.izquierdo != null) {
+		predecesor.padre.derecho = predecesor.izquierdo;
+		predecesor.izquierdo.padre = predecesor.padre;
+		predecesor = null;
+		elementos--;
+		return true;
+		}
+		
+		if(predecesor.izquierdo == null) {
+		predecesor.padre.derecho = null;
+		predecesor = null;
+		elementos--;
+		return true;
+		}
+		
+	}
+//Fin del programa	
+	return false;
 }
-*/
+
+//---------------------------------------------------------------------
+public Nodo buscaNodo(T x){
+	Nodo otroNodo = raiz; 	
+/**	if(otroNodo == raiz){
+		
+		return otroNodo;
+		
+	}	
+*/	
+	while(otroNodo != null){
+		if(otroNodo.elemento == x) {
+			return otroNodo;
+		}else if(x.compareTo(otroNodo.elemento) <= 0) {
+			otroNodo = otroNodo.izquierdo;
+		}else {
+			otroNodo = otroNodo.derecho;
+		}
+	}
+	
+	return null;
+
+}
+
 //----------------------------------------------------------------------
-/**public void bsf(Nodo node) {
-	if(node != null) {
-		Cola cola = new Cola();
-		Cola.push
+public void bsf(Nodo nodo) {
+	if(nodo == null) {
+		return;
+	}
+	
+	Queue<Nodo> nodos = new LinkedList<>();
+	nodos.add(raiz);
+	
+	while(!nodos.isEmpty()) {
+		Nodo node = nodos.remove();
+		System.out.print(" " + node.elemento);
+		if(node.izquierdo != null) {
+			nodos.add(node.izquierdo);
+		}
+		
+		if(node.derecho != null) {
+			nodos.add(node.derecho);
+		}
 	}
 }
-*/
+
 //-------------------------------------------------------------------------
-public boolean busca(int r) {
-	Nodo otroNodo = raiz; //creamos un nodo
+public boolean busca(T r) {
+	Nodo otroNodo = raiz; //creamos un nodo que va a la raiz
 	while(otroNodo != null) { // mientras no este vacio, seguimos, si no, regresamos vacio
 		if(otroNodo.elemento == r) {//revisamos en el nodo actual
 			return true;
-		}else if(r < otroNodo.elemento) {//revisamos en su nodo izquierdo
+		}else if( r.compareTo(otroNodo.elemento) <= 0) {//revisamos en su nodo izquierdo
 			otroNodo = otroNodo.izquierdo;
 		}else {
 			otroNodo = otroNodo.derecho;//revisamos en su nodo derecho
@@ -105,19 +251,15 @@ public void postorder(Nodo z) {
 		System.out.print(z.elemento + " ");
 	}
 }
-
-
-
-	
 	//------------------------------------------------
 	//Nodo del arbol
 	private class Nodo{
-		public int elemento;
+		public T elemento;
 		public Nodo izquierdo;
 		public Nodo derecho;
 		public Nodo padre;
 		
-		public Nodo(int elemento){
+		public Nodo(T elemento){
 			this.izquierdo = null;
 			this.derecho = null;
 			this.padre = null;
@@ -127,25 +269,33 @@ public void postorder(Nodo z) {
 	}
 //-----------------------------------------------------------
 	public static void main(String [] args) {
-		pruebasarboles tree = new pruebasarboles();
-		tree.agrega(6);
-		tree.agrega(4);
-		tree.agrega(8);
-		tree.agrega(5);
-		tree.agrega(3);
-		tree.agrega(7);
-		tree.agrega(9);
-		tree.agrega(1);
+		pruebasarboles <Integer> tree = new pruebasarboles <Integer>();
+		tree.agrega(50);
+		tree.agrega(30);
+		tree.agrega(20);
+		tree.agrega(45);
+		tree.agrega(15);
+		tree.agrega(25);
+		tree.agrega(35);
+		tree.agrega(48);
+		tree.agrega(24);
+//		tree.agrega(11);
 		
-		tree.recorrido_en_orden(tree.raiz);
-		System.out.println("");
+//		tree.recorrido_en_orden(tree.raiz);
+/**		System.out.println("");
 		tree.preorder(tree.raiz);
 		System.out.println("");
 		tree.postorder(tree.raiz);
 		System.out.println("");
 		System.out.println("el arbol tiene " + tree.elementos + " elementos");
-		System.out.println(tree.busca(1));
+*/
+//		System.out.println(tree.busca(0));
+//		System.out.println();
+		System.out.println(tree.elimina(30));
 		System.out.println();
+		tree.recorrido_en_orden(tree.raiz);
+		System.out.println();
+//		System.out.println(tree.predecesor(5));
 	}
 
 }
