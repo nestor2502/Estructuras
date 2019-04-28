d23f3806840e06a762673a070509e966250b345d
IsayDBS <42330467+IsayDBS@users.noreply.github.com>
2019-04-28 14:29:19 -0500

    Add files via upload

--- src/practica5/ArbolBinarioBusqueda.java
+++ src/practica5/ArbolBinarioBusqueda.java
@@ -1,7 +1,7 @@
-package practica5;
+
 
 /**
- * Clase que representa un Ã¡rbol binario de bÃºsqueda.
+ * Clase que representa un árbol binario de búsqueda.
  */
 public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<T>{
 
@@ -16,7 +16,12 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
 
     }
 
-
+//-----------------------------------------------------------------------------------------
+   /**Metodo que agrega un vertice al arbol
+    * @param elemento
+    * @return true; se agrego exitosamente
+    * 
+    */
     @Override
     public void agrega(T elemento){
 	      
@@ -54,7 +59,12 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
      
      elementos++; 
      }
-
+/**Metodo que revisa que se encuentra el vertice con el elemento
+ * @return true, el elemento se encuentra
+ * @return false, el elemento no se encuentra en un vertice
+ * @param elemento
+ * 
+ */
     @Override
     public boolean contiene(T elemento){
 	     Vertice otroNodo = raiz; //creamos un nodo
@@ -72,93 +82,124 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
          }
          return false;
      }
-
+/**
+ * Metodo que elimina un vertice del arbol
+ * @return true, el vertice se borro exitosamente
+ * @return false, el vertice no se encuentra
+ * @param j
+ */
     @Override
     public boolean elimina(T j) {
-	Vertice aux = buscaNodo(j);
-	if(raiz == null) {
-		return false;
-	}
-//------------------------------------------------------------------------------
-	//No tiene hijos
-	if(aux.izquierdo == null && aux.derecho == null) {
-		if(aux == raiz) {
-			raiz = null;
-			elementos--;
-			return true;
-		}
-		
-		if(aux.padre.izquierdo == aux)
-			aux.padre.izquierdo = null;
-		else
-			aux.padre.derecho = null;
-		return true;
-	
-	}
-//-------------------------------------------------------------------------------------------------
-//Tiene un nodo hijo
-	//Caso izquierdo
-	if(aux.derecho == null && aux.izquierdo != null) {
-		if(aux == raiz) { //Caso especial
-			raiz = raiz.izquierdo;
-			elementos--;
-			return true;
-		}
-		
-		
-		if(aux.padre.izquierdo == aux)
-			aux.padre.izquierdo = aux.padre.izquierdo;
-		else
-			aux.padre.derecho = aux.padre.izquierdo;
-		
-		return true;
+    	if(contiene(j) == false)
+    		return false;
+    	
+    	Vertice aux = buscaNodo(j);
+    	
+    	if(raiz == null) {
+    		return false;
+    	}
+    //------------------------------------------------------------------------------
+    	//No tiene hijos
+    	if(aux.izquierdo == null && aux.derecho == null) {
+    		if(aux == raiz) {
+    			raiz = null;
+    			elementos--;
+    			return true;
+    		}
+    		
+    		if(aux.padre.izquierdo == aux) {
+    			aux.padre.izquierdo = null;
+    		}else {
+    			aux.padre.derecho = null;
+    		}
+    		elementos--;
+    		return true;
+    	
+    	}
+    //-------------------------------------------------------------------------------------------------
+    //Tiene un nodo hijo
+    	//Caso izquierdo
+    	if(aux.derecho == null && aux.izquierdo != null) {
+    		if(aux == raiz) { //Caso especial
+    			raiz = raiz.izquierdo;
+    			elementos--;
+    			return true;
+    		}
+    		
+    		if(aux.padre.derecho == aux) {
+    		aux.izquierdo.padre =aux.padre;
+    		aux.padre.derecho = aux.izquierdo;
+    		}else {
+    		aux.izquierdo.padre = aux.padre;
+    		aux.padre.izquierdo = aux.izquierdo;
+    		
+    		}
+//    		aux.elemento = aux.izquierdo.elemento;
+//    		aux.izquierdo.padre = aux.padre;
+//    		aux.izquierdo = null;
+    		elementos--;
+    		return true;
 
-	
+    	
 
-	}
-	//Caso derecho
-//--------------------------------------------------------------------	
-	if(aux.izquierdo == null && aux.derecho != null) {
-		if(aux == raiz) {	//Caso especial
-			raiz = raiz.derecho;
-			elementos--;
-			return true;
-		}
-		
-		if(aux.padre.izquierdo == aux)
-			aux.padre.izquierdo = aux.padre.derecho;
-		else
-			aux.padre.derecho = aux.padre.derecho;
-		
-		return true;
+    	}
+    	//Caso derecho
+    //--------------------------------------------------------------------	
+    	if(aux.izquierdo == null && aux.derecho != null) {
+    		if(aux == raiz) {	//Caso especial
+    			raiz = raiz.derecho;
+    			elementos--;
+    			return true;
+    		}
+    		if(aux.padre.derecho == aux) {
+    		aux.derecho.padre =aux.padre;
+    		aux.padre.derecho = aux.derecho;
+    		}else {
+    		aux.derecho.padre = aux.padre;
+    		aux.padre.izquierdo = aux.derecho;
+    		
+    		}
+//    		aux.elemento = aux.derecho.elemento;
+//    		aux.derecho.padre = aux.padre;
+//    		aux.derecho = null;
+    		elementos--;
+    		return true;
 
-	}
-//--------------------------------------------------------------------
-	//Caso tiene 2 hijos
-	if(aux.izquierdo != null && aux.derecho != null) {
-		Vertice predecesor = predecesor(aux.elemento);
-		//Nodo temporal;
-		aux.elemento = predecesor.elemento;
-		if(predecesor.izquierdo != null) {
-		predecesor.padre.derecho = predecesor.izquierdo;
-		predecesor.izquierdo.padre = predecesor.padre;
-		predecesor = null;
-		elementos--;
-		return true;
-		}
-		
-		if(predecesor.izquierdo == null) {
-		predecesor.padre.derecho = null;
-		predecesor = null;
-		elementos--;
-		return true;
-		}
-		
-	}
-//Fin del programa	
-	return false;
-}
-
+    	}
+    //--------------------------------------------------------------------
+    	//Caso tiene 2 hijos
+    	if(aux.izquierdo != null && aux.derecho != null) {
+    		Vertice predecesor = predecesor(aux.elemento);	
+    //Caso raiz(especial)		
+    		if(aux == raiz) { //Se borra la raiz
+    			if(predecesor == raiz.izquierdo){ //Caso donde el predecesor esta a la ziquierda de la raiz
+    				raiz.izquierdo = null;
+    				raiz.elemento = predecesor.elemento;
+    			}else {//Caso donde el predecesor no esta a la izquierda de la raiz, 1)donde el predecesor no tiene hijo izquierdo, 2)predecesor tiene hijo izquierdo
+    				raiz.elemento = predecesor.elemento;
+    				if(predecesor.izquierdo != null){//Tiene hijo izquierdo
+    				predecesor.izquierdo.padre = predecesor.padre;
+    				predecesor.padre.derecho = predecesor.izquierdo;
+    			}else//No tiene hijo izquierdo
+    				predecesor.padre.derecho = null;
+    			}
+    		}else{//Caso General
+    			//Hy dos casos, el predecesor tiene hijo izquierdo null o no
+    			T temporal = predecesor.elemento;
+    			elimina(predecesor.elemento);
+    			aux.elemento = temporal;
+    		}		
+    		elementos--;
+    		return true;
+    	}
+    //Fin del programa	
+    	return false;
+    }
+/**
+ * Metodo que te regresa el vertice predecesor de cierto elemento
+ * @param y
+ * @return Vertice
+ */
  public Vertice predecesor(T y) {
 	Vertice encontrado = buscaNodo(y);
 	//arbol vacio
@@ -183,17 +224,12 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
 	}
 	//return null;
 }
+//-------------------------------------------------------------------------------------
 
 public Vertice buscaNodo(T x){
 	Vertice otroNodo = raiz; 	
-/**	if(otroNodo == raiz){
-		
-		return otroNodo;
-		
-	}	
-*/	
 	while(otroNodo != null){
-		if(otroNodo.elemento == x) {
+		if(otroNodo.elemento.compareTo(x)==0) {
 			return otroNodo;
 		}else if(x.compareTo(otroNodo.elemento) <= 0) {
 			otroNodo = otroNodo.izquierdo;
@@ -206,6 +242,12 @@ public Vertice buscaNodo(T x){
 
 }
 
+//------------------------------------------------------------------------------------------------
+/**
+ * Metodo que regresa el vertice mas a la derecha
+ * @param z
+ * @return Vertice 
+ */
 public Vertice maximo(T z) {
 	Vertice encontrado = buscaNodo(z);
 	while(encontrado.derecho != null) {
@@ -213,10 +255,13 @@ public Vertice maximo(T z) {
 	}
 	return encontrado;
 }
-
+//--------------------------------------------------------------------------------------------------
+/**
+ * Metodo que regresa la raiz del arbol
+ * @return
+ */
 public Vertice raiz(){
 
 	return raiz;
 }
 }
-
