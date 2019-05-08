22acd42842e8c90ad9197451a6c569ff4c10990c
nestor2502 <nestor2502@ciencias.unam.mx>
2019-05-08 10:19:11 -0500

    se termina monticulos

--- src/practica8/Test.java
+++ src/practica8/Test.java
@@ -6,8 +6,16 @@ public class Test{
 
 	 	int [] arr = {20, 4, 6, 2, 9, 3, 1};
         //int[] arr = new int[0];
+        System.out.println("primer monticulo");
 	 	Monticulo m1 = new Monticulo(arr);
 	 	m1.muestra();
+	 	int [] arr2= new int[arr.length];
+	 	/**
+	 	for(int i =0;i< arr2.length;i++){
+
+	 		arr2[i]=m1.elimina();
+	 	}
+
 	 	/**
 	 	Ordenamientos or = new Ordenamientos();
 	 	or.heapSort(arr);
@@ -18,10 +26,40 @@ public class Test{
 	 	System.out.println();
 
 	 	*/
-	 	for(int i:arr){
-
+	 	System.out.println();
+	 	System.out.println();
+	 	System.out.println("se elimina la raiz");
+	 	//m1.agrega(0);
+	 	//m1.muestra();
 	 	System.out.println(m1.elimina());
-	 	}
+	 	System.out.println("elementos restantes");
+        m1.muestra();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+	 	System.out.println();
+	 	System.out.println("se elimina la raiz");
+	 	//m1.agrega(0);
+	 	//m1.muestra();
+	 	System.out.println(m1.elimina());
+	 	System.out.println("elementos restantes");
+        m1.muestra();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+	 	System.out.println();
+	 	System.out.println("se elimina la raiz");
+	 	//m1.agrega(0);
+	 	//m1.muestra();
+	 	System.out.println(m1.elimina());
+	 	System.out.println("elementos restantes");
+        m1.muestra();
+        System.out.println();
+        System.out.println();
+	 	/**for(int i:arr2){
+
+	 	System.out.println(i);
+	 	}*/
 	 	//int a = m1.elimina();
 	 	//m1.muestra();
 	 	//System.out.println();
