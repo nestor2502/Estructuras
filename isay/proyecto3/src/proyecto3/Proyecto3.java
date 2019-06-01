49aa3bb1786940e5e1dca9ba4301521b46e917e5
IsayDBS <42330467+IsayDBS@users.noreply.github.com>
2019-06-01 14:22:35 -0500

    Add files via upload

--- isay/proyecto3/src/proyecto3/Proyecto3.java
+++ isay/proyecto3/src/proyecto3/Proyecto3.java
@@ -3,8 +3,37 @@ package proyecto3;
 
 public class Proyecto3 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        Equipos Seoul = new Equipos("Seoul Dynasty", 200, "Seoul.png");
+        Equipos Houston = new Equipos("Houston Outlaws", 180, "Houston.png");
+        Equipos LAG = new Equipos("LA Gladiator", 210, "LosAngeles.png");
+        Equipos NY = new Equipos("New York Excelsior", 240, "New York.png");
+        Equipos Phily = new Equipos("Philadelphia Fusion", 230, "Philadelphia.png");
+        Equipos Shangai = new Equipos("Shangai Dragons", 190, "Shangai.png");
+        Equipos Vanc = new Equipos("Vancouver Titans", 215,"Vancouver.png");
+        Equipos Atl = new Equipos("Atlanta Reign", 224, "Atlanta.png");
+        Equipos[] arr = {Seoul,Houston,LAG,NY,Phily,Shangai,Vanc,Atl};
+        Torneo1 test = new Torneo1(arr);
+        test.cuartos();
+        Thread.sleep(2000);
+        test.mostrarCuartos();
+        test.semifinal();
+        Thread.sleep(2000);
+        test.mostrarSemifinal();
+        test.last();
+        Thread.sleep(2000);
+        test.mostrarLast();
+        /**
+        test.iniciar();
+        Thread.sleep(4000);
+        test.mostrarCuartos();
+        Thread.sleep(4000);
+        test.mostrarSemifinal();
+        Thread.sleep(4000);
+        test.mostrarLast();
+        //Thread.sleep(4000);
+        //test.mostrarCuartos();
+        * */
     }
     
 }
